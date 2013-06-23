1d72d997aa132cf45bffde923d182d5d06dc0f9f
mreiben <jason.eiben@gmailcom>
2013-06-23 16:13:09 -0400

    add jquery to 37 signals page

---
+++ projects_page/37signals/application.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	$("#basecamp-box").on("mouseenter", function() {
+		event.preventDefault();
+		$("#banner").hide();
+		$("#basecamp-banner").show();
+	});
+	$("#basecamp-box").on("mouseleave", function() {
+		event.preventDefault();
+		$("#basecamp-banner").hide();
+		$("#banner").show();
+	});
+	$("#highrise-box").on("mouseenter", function() {
+		event.preventDefault();
+		$("#banner").hide();
+		$("#highrise-banner").show();
+	});
+	$("#highrise-box").on("mouseleave", function() {
+		event.preventDefault();
+		$("#highrise-banner").hide();
+		$("#banner").show();
+	});
+	$("#campfire-box").on("mouseenter", function() {
+		event.preventDefault();
+		$("#banner").hide();
+		$("#campfire-banner").show();
+	});
+	$("#campfire-box").on("mouseleave", function() {
+		event.preventDefault();
+		$("#campfire-banner").hide();
+		$("#banner").show();
+	});
+});
