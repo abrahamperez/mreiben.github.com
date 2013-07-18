dc01a1fd716e41731bd21cc418a27fc9804772a2
mreiben <jason.eiben@gmailcom>
2013-07-18 00:29:40 -0400

    update color script for shopping list

--- projects_page/shoppinglist/application.js
+++ projects_page/shoppinglist/application.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
+$(document).ready(function() {
 //Make list sortable
-	$(function() {
-		$("#items_list").sortable({
-			placeholder: "ui-sortable-placeholder"
-		});
-	});
+	function ui_sort() {
+		$("#items_list").sortable({placeholder: "ui-sortable-placeholder"});
+	}
+	ui_sort();
+//List item arrays
 
 //Add item through click
 	$("#add_button").on("click", function() {
@@ -13,7 +13,7 @@ $(document).ready(function(){
 		var listItem = document.createElement("li");
 		listItem.id = new_item;
 		listItem.className = "ui-state-default";
-		listItem.innerHTML= '<input type="checkbox" class="checkbox" value="'+new_item+'">'+new_item+' <select class="item_type" onchange="type_select()"><option value="undefined">Type...</option><option value="groceries">Groceries</option><option value="household supplies">Supplies</option><option value="clothing">Clothing</option><option value="entertainment">Entertainment</option></select> $<input type="text" class="clicker" class="item_price" size="6" placeholder="Price..." autocomplete="off">';
+		listItem.innerHTML= '<input type="checkbox" class="checkbox" value="'+new_item+'">'+new_item+' <select class="item_type"><option value="undefined">Type...</option><option value="groceries">Groceries</option><option value="supplies">Supplies</option><option value="clothing">Clothing</option><option value="entertainment">Entertainment</option></select> $<input type="text" class="item_price" size="6" placeholder="Price..." autocomplete="off">';
 		$("#list_holder ul").append(listItem);
 		$("#item_input").val("");
 	});
@@ -22,42 +22,31 @@ $(document).ready(function(){
 		if(event.keyCode == 13) {
 			event.preventDefault();
 			$("#add_button").click();
-		};
+		}
 	};
 //Delete selected items
 	$("#clear_button").on("click", function() {
 		event.preventDefault();
 		$('.checkbox').filter(':checked').parent().remove();
 	});
+//Add colors to type
+	var x = $('.item_type')
+	$('ul').on('change', 'li select', (function() {
+		var clicked = this;
+		if($(clicked).val() === "undefined"){
+			$(clicked).parent().css({'background-color':'#ffffff'});
+			}
+		else if($(clicked).val() === "groceries"){
+			$(clicked).parent().css({'background-color':'#85E085'});
+			}
+		else if($(clicked).val() === "supplies"){
+			$(clicked).parent().css({'background-color':'#71C6FF'});
+			}
+		else if($(clicked).val() === "clothing"){
+			$(clicked).parent().css({'background-color':'#FFFF99'});
+			}
+		else if($(clicked).val() === "entertainment"){
+			$(clicked).parent().css({'background-color':'#FF8080'});
+			}
+		}))
 });
-/*Add colors to type	
-	select.onchange = function type_select(){
-		var type = this;
-		if (type.value.selectedIndex == 0){
-			type.parent().parent().css({background-color:#ffffff, opacity:0.5});
-			};
-		else if(type == 1) {
-			type.parent().parent().css({background-color:#00cc00, opacity:0.5});
-			};
-		else if(type == 2) {
-			type.parent().parent().css({background-color:#0066ff, opacity:0.5});
-			};
-		else if(type == 3) {
-			type.parent().parent().css({background-color:#ff9900, opacity:0.5});
-			};
-		else if(type == 4) {
-			type.parent().parent().css({background-color:#ff3300, opacity:0.5});
-			};*/
-
-	/*Hover over list items to reveal drop-down for item type
-	$("li").on("mouseenter", function(){
-		event.preventDefault();
-		$(this).children().css("display", "inline");
-	})
-	$("li").on("mouseleave", function(){
-		event.preventDefault();
-		$(this).children(".item_type").css("display", "none");
-		$(this).children(".item_price").css("display", "none");
-	})*/
-	
-
