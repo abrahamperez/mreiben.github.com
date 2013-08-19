0c5b057067a84114cfa8f1970faa6d2e3dca55e5
mreiben <jason.eiben@gmail.com>
2013-08-19 19:47:49 -0400

    update image translation page

--- projects_page/image_translation/application.js
+++ projects_page/image_translation/application.js
@@ -1,40 +1,50 @@
 $(document).ready(function() {
+    var search_array = [];
+    var api_array = [];
+    var url_array = [];
     $('#submit_button').on('click', function(){
         event.preventDefault();
-        var search_term = $('#user_input').val().toLowerCase();
-        var array = search_term.split(" ");
-        var search_array = [];
-        for (var i in array) {
-            if (i != "i" && i != "he" && i != "she" && i != "it" && i != "you" && i != "is" && i != "was" && i != "am" && i != "are" && i != "were" && i != "a" && i != "an" && i != "if" && i != "when" && i != "where" && i != "the") {
-                search_array.push(array[i]);
-            };
-        };
-        //var search_array = array.reverse();
-        console.log(search_array);
+        search_array = [];
+        api_array = [];
+        url_array = [];
         $('#status_holder').css("display", "inline");
         $('#image_canvas').html("");
-        //for (var i in search_array) {
-            //$.getJSON("https://api.instagram.com/v1/tags/"+i+"/media/recent?client_id=d437c029184f4946934704f5957c114d&callback=?", function(json){
-            //console.log(json);
-            //var url = json.data[0].images.standard_resolution.url;
-            //$('#image_canvas').html("<img src='"+url+"' class='photo'>");
-            //});
-        //}
-        for (var i in search_array) {
-            var flickr_url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=bdfe5cec66a44b4617d2b096f5770cb0&text="+search_array[i]+"&format=json&jsoncallback=?";
-            console.log(search_array[i]);
-            $.getJSON(flickr_url).done(function(json){
-                console.log(json);
+        var search_term = $('#user_input').val().toLowerCase();
+        var array = search_term.split(" ");
+        for (var i in array) {
+            var term = array[i];
+            if (term != "i" && term != "he" && term != "she" && term != "it" && term != "you" && term != "is" && term != "was" && term != "am" && term != "are" && term != "were" && term != "a" && term != "an" && term != "if" && term != "when" && term != "where" && term != "the") {
+                search_array.push(array[i]);
+            }
+        }
+        for (var k in search_array) {
+            var flickr_url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=bdfe5cec66a44b4617d2b096f5770cb0&text="+search_array[k]+"&format=json&jsoncallback=?";
+            api_array.push(flickr_url);
+        }
+        console.log(api_array);
+        for (var j in api_array) {
+            console.log(j, api_array[j]);
+            var photo_url = 0;
+            $.getJSON(api_array[j], function(json){
                 var x = Math.floor((Math.random()*10)+1);
                 var photo = json.photos.photo[x];
                 var photo_farm = photo.farm;
                 var photo_server = photo.server;
                 var photo_id = photo.id;
                 var photo_secret = photo.secret;
-                var photo_url = "http://farm"+photo_farm+".staticflickr.com/"+photo_server+"/"+photo_id+"_"+photo_secret+".jpg";
+                photo_url = "http://farm"+photo_farm+".staticflickr.com/"+photo_server+"/"+photo_id+"_"+photo_secret+".jpg";
+                console.log(photo_url);
+            })
+            .done(function(){
+                url_array.push(photo_url);
+                if (url_array.length == search_array.length) {
+                console.log(url_array);
+                for (var z in url_array) {
+                $('#image_canvas').append("<img class='photo' src='"+url_array[z]+"' id='image"+z+"' alt='image' />")
                 $('#status_holder').css("display", "none");
-                $('#image_canvas').append("<img src='"+photo_url+"' class='photo' id='"+search_array[i]+"'>");
-            });
+                }
+            }
+        })
         }
     });
 });
