1c1cd568f9d06ff400f33c6119b6a50f4e46e1f5
mreiben <jason.eiben@gmail.com>
2013-08-22 17:40:18 -0400

    update js

--- projects_page/image_translation/application2.js
+++ projects_page/image_translation/application2.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     var api_array = [];
     var url_array = [];
     var flickr_url = 0;
+    var photo_url = 0;
     $('#submit_button').on('click', function(){
         event.preventDefault();
         search_array = [];
@@ -15,25 +16,27 @@ $(document).ready(function() {
         for (var i = 0; i < array.length; i++) {
             var term = array[i];
             if (term != "i" && term != "he" && term != "she" && term != "it" && term != "you" && term != "is" && term != "was" && term != "am" && term != "are" && term != "were" && term != "a" && term != "an" && term != "if" && term != "when" && term != "where" && term != "the") {
-                search_array.push(array[i]);
+                flickr_url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=bdfe5cec66a44b4617d2b096f5770cb0&text="+term+"&format=json&jsoncallback=?";
+                url_array.push(flickr_url);
             }
         }
-        for (var k = 0; k < search_array.length; k++) {
-            flickr_url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=bdfe5cec66a44b4617d2b096f5770cb0&text="+search_array[k]+"&format=json&jsoncallback=?";
-            img_id = "img_"+k;
-            console.log(img_id);
-            $.when($.getJSON(flickr_url, function(json){
-            console.log(img_id);
-            var x = Math.floor((Math.random()*10)+1);
-            var photo = json.photos.photo[x];
-            var photo_farm = photo.farm;
-            var photo_server = photo.server;
-            var photo_id = photo.id;
-            var photo_secret = photo.secret;
-            photo_url = "http://farm"+photo_farm+".staticflickr.com/"+photo_server+"/"+photo_id+"_"+photo_secret+".jpg";
-            })).done(function(){
-                $('#image_canvas').append("<img class='photo' src='"+photo_url+"' id='"+img_id+"' alt='image' />");
-            });
+        console.log(url_array);
+        for (var j = 0; j < url_array.length; j++) {
+            $.ajax({
+                url: url_array[j],
+                type: 'get',
+                dataType: 'json',
+                async: false,
+                success: function (json) {
+                    var x = Math.floor((Math.random()*10)+1);
+                    var photo = json.photos.photo[x];
+                    var photo_farm = photo.farm;
+                    var photo_server = photo.server;
+                    var photo_id = photo.id;
+                    var photo_secret = photo.secret;
+                    photo_url = "http://farm"+photo_farm+".staticflickr.com/"+photo_server+"/"+photo_id+"_"+photo_secret+".jpg";
+                }
+            }).done($('#image_canvas').append("<img class='photo' src='"+photo_url+"' alt='image' />"));
         }
     })
 });
