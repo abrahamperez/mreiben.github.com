9331558e80faef7993a7c56761c61c836a5fa5d8
mreiben <jason.eiben@gmail.com>
2013-08-22 20:08:41 -0400

    change input placeholder

--- projects_page/image_translation/application2.js
+++ projects_page/image_translation/application2.js
@@ -41,6 +41,5 @@ $(document).ready(function() {
         }
         $('#image_canvas').delay(2000).fadeIn(750);
         $('#progress').delay(1000).fadeOut(750);
-        $('input').val([]);
     });
 });
